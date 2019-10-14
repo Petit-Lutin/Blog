17a3ff9c8c97e29ad32e37324c84a05efbf79d6d
Petit-Lutin <laurence.platzer@wanadoo.fr>
2019-10-14 16:27:52 +0200

    mise en page bootstrap page article

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,85 +1,122 @@
 <?php ob_start();
 ?>
-<div class="contenuPage">
+<div class="contenuPage" xmlns="http://www.w3.org/1999/html">
     <div class="news">
-        <h3>
-            <a href="index.php?action=article&amp;id=<?= $post['id'] ?>"> <?= htmlspecialchars($post['title']) ?></a>
-            <em>le <?= $post['creation_date_fr'] ?></em>
-        </h3>
-        <p><a href="index.php?action=modifier-article&amp;id=<?= $post['id'] ?>">Modifier</a></p>
-        <p>
-            <?= nl2br($post['content']) ?>
-        </p>
-        slug de cette page = " <?= $post['slug'] ?>"
-    </div>
-    <?php
-
-
-    //todo: cacher l'affichage du bouton précédent quand c'est le dernier article
-    if ((null != $post['id'] + 1)) {
-        $biggerId = $post['id'] + 1; ?>
-
-        <p><a href="index.php?action=article&amp;id=<?= $biggerId ?>">Post précédent (+ récent)</a></p>
-        <?php
-    }
-
-    if (null != $post['id'] - 1) {
-        $smallerId = $post['id'] - 1;
-        ?>
-        <p><a href="index.php?action=article&amp;id=<?= $smallerId ?>">Post suivant (+ ancien)</a></p>
-        <?php
-    }
-    ?>
-    <p><a href="index.php?action=liste-articles&page=0">Retour à la liste des articles</a></p>
-
-
-    <h2>Commentaires</h2>
-    <!--On affiche les commentaires existants -->
-    <?php
-    while ($comment = $comments->fetch()) {
-        ?>
-        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-        <?php
-        // si le commentaire en question a déjà été signalé par un utilisateur (reconnu par son adresse IP), on n'affiche pas de bouton "signaler"
-        if ($comment['reported'] == 1) {
-            ?>
-            <div class="reportedComment">Vous avez signalé ce commentaire.</div>         <hr>
-
-            <?php
-            // autrement on affiche un bouton "signaler"
-        } else {
-            ?>
-            <p>
-                <button class="btn btn-warning btn-reportComment"><a
-                            href="index.php?action=report-comment&amp;commentid=<?= $comment['id'] ?>"
-                            title="Si ce commentaire vous semble injurieux ou offensant, vous pouvez demander à ce qu'il soit modéré par Jean Forteroche.">Signaler</a>
-                </button>
-            </p>
-            <div class="deletedComment">Ce commentaire a été supprimé car il contenait des propos injurieux ou
-                offensants.
+        <div class="card border-secondary mb-3">
+            <div class="card-header">
+                <h4 class="card-title">
+                    <a href="index.php?action=article&amp;id=<?= $post['id'] ?>"> <?= htmlspecialchars($post['title']) ?></a>
+                    <small class="card-subtitle mb-2 text-muted">- le <?= $post['creation_date_fr'] ?></small>
+                </h4>
             </div>
-            <?php
-        }
-    } ?>
+
+            <div class="card-body">
+
+                <div class="card-text">
+
+                    <p class="text-right">
+                        <button class="btn btn-info disabled"><a class="card-link"
+                                    href="index.php?action=modifier-article&amp;id=<?= $post['id'] ?>">Modifier</a>
+                        </button>
+                    </p>
 
 
-    <strong>Ajouter un commentaire :</strong>
-    <!-- Pour poster un commentaire -->
-    <form action="index.php?action=add-comment&amp;id=<?= $post['id'] ?>" method="post">
-        <div>
-            <label for="author">Auteur</label><br/>
-            <input type="text" id="author" name="author" required/>
-        </div>
-        <div>
-            <label for="comment">Commentaire</label><br/>
-            <textarea id="comment" name="comment" required></textarea>
-        </div>
-        <div>
-            <input type="submit"/>
-        </div>
-    </form>
+                    <p>
+                        <?= nl2br($post['content']) ?>
+                    </p>
+                    slug de cette page = " <?= $post['slug'] ?>"
 
-    <?php $content = ob_get_clean(); ?>
-</div>
-<?php require('template.php'); ?>
+                    <?php
+                    //todo: cacher l'affichage du bouton précédent quand c'est le dernier article
+                    if ((null != $post['id'] + 1)) {
+                        $biggerId = $post['id'] + 1; ?>
+
+                        <p><a href="index.php?action=article&amp;id=<?= $biggerId ?>">Post précédent (+ récent)</a></p>
+                        <?php
+                    }
+
+                    if (null != $post['id'] - 1) {
+                        $smallerId = $post['id'] - 1;
+                        ?>
+                        <p><a href="index.php?action=article&amp;id=<?= $smallerId ?>">Post suivant (+ ancien)</a></p>
+                        <?php
+                    }
+                    ?>
+
+                </div>
+            </div>
+
+            <div class="modal-header"><h4>Commentaires</h4></div>
+            <div class="modal-body">
+                <ul class="list-group list-group-flush">
+
+                    <!--                <div class="card-text">-->
+
+                    <!--On affiche les commentaires existants -->
+                    <?php
+                    while ($comment = $comments->fetch()) {
+                        ?>
+                        <li class="list-group-item  ">
+                        <p><strong><?= htmlspecialchars($comment['author']) ?></strong>
+                            le <?= $comment['comment_date_fr'] ?>
+                        </p>
+                        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+                        <?php
+                        // si le commentaire en question a déjà été signalé par un utilisateur (reconnu par son adresse IP), on n'affiche pas de bouton "signaler"
+                        if ($comment['reported'] == 1) {
+                            ?>
+                            <div class="reportedComment">Vous avez signalé ce commentaire.</div>
+                            <!--                        <hr>-->
+
+                            <?php
+                            // autrement on affiche un bouton "signaler"
+                        } else {
+                            ?>
+                            <p>
+                                <button class="btn btn-warning disabled btn-reportComment "><a
+                                            href="index.php?action=report-comment&amp;commentid=<?= $comment['id'] ?>"
+                                            title="Si ce commentaire vous semble injurieux ou offensant, vous pouvez demander à ce qu'il soit modéré par Jean Forteroche.">Signaler</a>
+                                </button>
+                            </p>
+                            <div class="deletedComment">Ce commentaire a été supprimé car il contenait des propos
+                                injurieux
+                                ou
+                                offensants.
+                            </div>
+                            </li>
+                            <?php
+                        }
+                    } ?>
+                    <!--                </ul>-->
+
+                    <!--                </div>-->
+
+                    <li class="list-group-item">
+                        <h5>Ajouter un commentaire :</h5>
+                        <!-- Pour poster un commentaire -->
+                        <form action="index.php?action=add-comment&amp;id=<?= $post['id'] ?>" method="post">
+                            <div>
+                                <label for="author">Votre nom ou pseudo</label><br/>
+                                <input type="text" id="author" name="author" required/>
+                            </div>
+                            <div>
+                                <label for="comment">Commentaire</label><br/>
+                                <textarea id="comment" name="comment" required></textarea>
+                            </div>
+                            <div>
+                                <input type="submit"/>
+                            </div>
+                        </form>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class="card-footer">
+                <p><a href="index.php?action=liste-articles&page=0">Retour à la liste des articles</a></p>
+
+            </div>
+        </div>
+        <?php $content = ob_get_clean(); ?>
+
+        <?php require('template.php'); ?>
