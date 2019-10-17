c2815afc6bd7372e4947317235b1cba6f4b13420
Petit-Lutin <laurence.platzer@wanadoo.fr>
2019-10-17 15:43:12 +0200

    réparation mise en page liste articles

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -15,76 +15,72 @@
     <?php
     while ($data = $posts->fetch()) //
     {
-    ?>
+        ?>
 
-    <div class="news">
+        <div class="news">
 
-        <div class="card border-secondary mb-3">
-            <div class="card-header">
-                <h4 class="card-title">
-                    <a class="card-link"
-                       href="index.php?action=article&amp;id=<?= $data['id'] ?>&amp;slug=<?= $data['slug'] ?>"><?= htmlspecialchars($data['title']) ?></a>
-                    <small class="card-subtitle mb-2 text-muted">- le <?= $data['creation_date_fr'] ?></small>
-                </h4>
-            </div>
-
-            <div class="card-body">
-                <?php
-                if (isset($_SESSION['email'])) {
-                    ?>
-                    <p class="text-right">
-                        <button class="btn btn-outline-info nav-item"><a
-                                                                 href="index.php?action=modifier-article&amp;id=<?= $data['id'] ?>">Modifier</a>
-                        </button>
-                    </p>
-                <?php }
-                ?>
-
-                <div class="card-text">
-                    <?= $data['content'] ?><!-- pas d htmlspecialchars pour la mise en forme -->
-                </div>
-
-            </div>
-
-            <div class="card-footer">
-                <div class="text-right">
-                    <em>
+            <div class="card border-secondary mb-3">
+                <div class="card-header">
+                    <h4 class="card-title">
                         <a class="card-link"
-                           href="index.php?action=article&amp;id=<?= $data['id'] ?>">Commentaires</a>
-                    </em>
+                           href="index.php?action=article&amp;id=<?= $data['id'] ?>&amp;slug=<?= $data['slug'] ?>"><?= htmlspecialchars($data['title']) ?></a>
+                        <small class="card-subtitle mb-2 text-muted">- le <?= $data['creation_date_fr'] ?></small>
+                    </h4>
                 </div>
+
+                <div class="card-body">
+                    <?php
+                    if (isset($_SESSION['email'])) {
+                        ?>
+                        <p class="text-right">
+                            <button class="btn btn-outline-info nav-item"><a
+                                        href="index.php?action=modifier-article&amp;id=<?= $data['id'] ?>">Modifier</a>
+                            </button>
+                        </p>
+                    <?php }
+                    ?>
+
+                    <div class="card-text">
+                        <?= $data['content'] ?><!-- pas d htmlspecialchars pour la mise en forme -->
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+                    <div class="text-right">
+                        <em>
+                            <a class="card-link"
+                               href="index.php?action=article&amp;id=<?= $data['id'] ?>">Commentaires</a>
+                        </em>
+                    </div>
+                </div>
+
             </div>
-
         </div>
-    </div>
 
-<!--    <div class="row">-->
-<!--        <div class="col-sm-6">-->
-            <?php
-            }
-            if ($page >= 1) {
-                ?>
-                <div class="text-left">
-                    <a href="liste-articles/page-<?= $page - 1 ?>">page précédente</a></div>
+        <?php
+    }
+    if ($page >= 1) {
+        ?>
+        <div class="text-left">
+            <a href="liste-articles/page-<?= $page - 1 ?>">page précédente</a>
+        </div>
 
-                <?php
-            } ?>
-<!--        </div>-->
-<!---->
-<!--        <div class="col-sm-6">-->
+        <?php
+    } ?>
 
-            <?php
+    <?php
 
-            if ($posts->rowCount() === 5) {
-                ?>
-                <div class="text-right"><a href="liste-articles/page-<?= $page + 1 ?>">page
-                        suivante</a></div>
+    if ($posts->rowCount() === 5) {
+        ?>
 
-            <?php }
-            ?>
-<!--        </div>-->
-<!---->
-<!--    </div>-->
+        <div class="text-right">
+            <a href="liste-articles/page-<?= $page + 1 ?>">page
+                suivante</a>
+        </div>
+
+    <?php }
+    ?>
 </div>
 
 
