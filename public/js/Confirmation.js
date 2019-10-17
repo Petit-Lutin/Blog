8cc5f8a4d90e944ee289152ff898e85e7bac8247
Petit-Lutin <laurence.platzer@wanadoo.fr>
2019-10-17 16:21:31 +0200

    ajout fichier JS séparé pour boîte dialogue confirmation

---
+++ public/js/Confirmation.js
@@ -0,0 +1,17 @@
+class Confirmation {
+    constructor(conteneur) {
+        this.conteneur = conteneur;
+        this.conteneur = document.querySelectorAll("confirmation");
+        this.message = "";
+    }
+}
+
+this.conteneur.addEventListener("click", this.message, (e) => {
+    if (!confirm(this.message)) {
+        e.preventDefault();
+        e.stopPropagation();
+
+    }
+})
+
+const confirmation1 = new Confirmation("confirmation");
