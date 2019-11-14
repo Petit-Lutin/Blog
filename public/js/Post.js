b3252fb677a35cb7e494a53c56724d7cd86904dc
Petit-Lutin <laurence.platzer@wanadoo.fr>
2019-11-14 17:28:17 +0100

    correction erreur dans JS pour validation slug

--- public/js/Post.js
+++ public/js/Post.js
@@ -1,25 +1,25 @@
 const validation = document.querySelectorAll(".formWithSlug");
+const titles = document.querySelectorAll(".title");
+const slugs = document.querySelectorAll(".slug");
 
 for (i = 0; i < validation.length; i++) {
-    const title = document.querySelectorAll(".title");
-    const slug = document.querySelectorAll(".slug");
-
+   let title=titles[i];
+   let slug=slugs[i];
 
    validation[i].addEventListener("click", (e) => {
         e.preventDefault();
         e.stopPropagation();
-
         let message = "";
-        //On vérifie avec une expression régulière que le slug est composé de lettres ou de chiffres et de tirets, sans espace ni accent
-        var regex = /^[a-z0-9\-]{2,}$/
 
-        if (title.trim().length === 0) message += "Le titre de l'article doit être complété.\r\n";
-        if ((slug.trim().length === 0) || !(regex.test(slug.value))){
+        if (title.value.trim().length === 0) message += "Le titre de l'article doit être complété.\r\n";
+
+       //On vérifie avec une expression régulière que le slug est composé de lettres ou de chiffres et de tirets, sans espace ni accent
+       var regex = /^[a-z0-9\-]{2,}$/
+        if ((slug.value.trim().length === 0) || !(regex.test(slug.value))){
             message += "L'URL personnalisée ne doit contenir que des lettres sans accent. Pour séparer les mots, vous pouver utiliser des tirets. Exemple d'URL personnalisée valide 'chapitre-4'.\r\n";
         }
         if (message.length > 0) {
             alert(message);
         }
-
     })
 }
